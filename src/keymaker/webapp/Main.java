4ae2d1f70a2abf47b955a61faf6e99339bec4603
Maarten van der Aart <maarten@buttonbasher.org>
2015-11-11 16:24:42 +0100

    Added properties file
    We can now configure the behaviour of keymaker wit a properties file.

--- src/keymaker/webapp/Main.java
+++ src/keymaker/webapp/Main.java
@@ -7,20 +7,40 @@ import keymaker.util.Password;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import org.apache.commons.configuration.*;
+import org.apache.commons.configuration.ConfigurationException;
 import java.io.FileWriter;
 
 
 class Main {
     private static final Logger logger = LogManager.getLogger(Main.class.getName());
+    private static int passwordLength = 12;
+    private static String outputDir = "outputDir";
+
+
 
     public static void main(String[] args) {
+        try {
+            Configuration config = new PropertiesConfiguration("keymaker.properties");
+            logger.info("Found \"keymaker.properties\"...");
+            passwordLength = config.getInt("password.length");
+            logger.info("Password length now set to: " + passwordLength);
+            outputDir = config.getString("output.directory");
+            logger.info("Output will be written to: " + outputDir);
+        }
+        catch(ConfigurationException cex)
+        {
+            logger.error(cex.toString(), "Unable to load \"keymaker.properties\"");
+            logger.warn("Using default value for password length: " + passwordLength);
+            logger.warn("Using default value for output directory: " + outputDir);
+        }
+
         get("updatepassword/:username/:servername", (request, response) -> {
-            Password password = new Password(request.params(":username"), request.params(":servername"), 60);
+            Password password = new Password(request.params(":username"), request.params(":servername"), passwordLength);
             String usermodString = "usermod -p '" + password.toCrypt() + "' " + password.getUsername() +"\n";
             logger.info("server " + request.ip() + " requested a password change for user " + password.getUsername() +
                     " on " + password.getServername());
-            //[TODO] Make the output directory configurable
-            String filename = "outputDir/" + password.getUsername() + "@" + password.getServername() + ".txt";
+            String filename = outputDir + "/" + password.getUsername() + "@" + password.getServername() + ".txt";
             try (FileWriter file = new FileWriter(filename, true)){
                 file.write(password.toString() + "\n");
                 logger.info("wrote new password to " + filename);
